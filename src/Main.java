fb769ff64d224151fbd402aa50c323565b01e6ac
v-zhou <v-zhou@hotmail.com>
2017-12-05 17:19:27 +0800

    1 第一周编程作业

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // TODO Auto-generated method stub
-        int foot;
-        double inch;
+        int c;
+        int f;
         Scanner in = new Scanner(System.in);
-        foot = in.nextInt();
-        inch = in.nextDouble();
-        int meter = (int)((foot+inch/12.0)*0.3048*100);
-        System.out.println("foot="+foot+",inch="+inch);
-        System.out.println(meter);
-
+        f = in.nextInt();
+        c = (int)( (f - 32) * 5 / 9.0 );
+        System.out.println(c);
 
     }
 }
