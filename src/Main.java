c97bfa5c5281b0f89d6eb070dc253ae9bc0d505d
v-zhou <v-zhou@hotmail.com>
2017-12-05 19:05:08 +0800

    java进阶 0.02 对象

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
 
-        int[] a1 = {1, 2, 3, 4, 5};
-        int[] a2 = a1;
-        for (int i = 0; i < a2.length; ++i) {
-            a2[i] ++;
-        }
-        for (int i = 0; i < a1.length; ++i) {
-            System.out.println(a1[i]);
-        }
+//        int[] a1 = {1, 2, 3, 4, 5};
+//        int[] a2 = a1;
+//        for (int i = 0; i < a2.length; ++i) {
+//            a2[i] ++;
+//        }
+//        for (int i = 0; i < a1.length; ++i) {
+//            System.out.println(a1[i]);
+//        }
 
         /// 5  16  16  普通变量是"所有者"(真拷贝 赋值)   数组变量是"管理者"(指针拷贝赋值)
         /*
@@ -24,5 +24,31 @@ public class Main {
         * 数组变量之间的赋值是管理权限的赋予
         * 数组变量之间的比较是判断是否管理同一个数组
          */
+
+//        int[] a = {1,2,3};
+//        int[] b = a;
+//        int[] a = {1,2,3};
+//        int[] b = {1,2,3};
+
+//        int[] a = {1,2,3};
+//        int[] b = new int[a.length];
+//        for (int i = 0; i < b.length; i++) {
+//            b[i] = a[i];
+//        }
+//        for (int i = 0; i < b.length; i++) {
+//            System.out.println(b[i]);
+//        }
+//        System.out.println(a==b);
+
+        int[] a = {1,2,3};
+        int x = in.nextInt();
+        boolean found = false;
+        for ( int k: a) {
+            if (k == x) {
+                found = true;
+            }
+        }
+        System.out.println(found);
+
     }
 }
