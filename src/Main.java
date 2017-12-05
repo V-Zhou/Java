b4e885d31b1a421303efba3bb130b9ba461953db
v-zhou <v-zhou@hotmail.com>
2017-12-05 18:47:59 +0800

    java进阶 1.1数组   数组变量与普通变量的关系

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,21 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
 
-        int[] a = new int[10];
-        a[0] = 5;
-        int[] b = a;
-        System.out.println(a[0]);
-        b[0] = 16;
-        System.out.println(a[0]);
-        System.out.println(b[0]);
+        int[] a1 = {1, 2, 3, 4, 5};
+        int[] a2 = a1;
+        for (int i = 0; i < a2.length; ++i) {
+            a2[i] ++;
+        }
+        for (int i = 0; i < a1.length; ++i) {
+            System.out.println(a1[i]);
+        }
 
         /// 5  16  16  普通变量是"所有者"(真拷贝 赋值)   数组变量是"管理者"(指针拷贝赋值)
+        /*
+        * 数组变量是数组的管理者而非数组本身
+        * 数组必须创建出来然后交给数组变量来管理
+        * 数组变量之间的赋值是管理权限的赋予
+        * 数组变量之间的比较是判断是否管理同一个数组
+         */
     }
 }
