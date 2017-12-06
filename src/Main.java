74400c0879cb5edb6d2b29954a4c8acc3c9a64b9
v-zhou <v-zhou@hotmail.com>
2017-12-06 09:18:35 +0800

    java进阶 第一周 类和对象 1.1定义类

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 
+
 import java.util.Scanner;
 
 public class Main {
@@ -7,48 +8,12 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        // 首字母大写的是包裹类型
 
-//        int[] a1 = {1, 2, 3, 4, 5};
-//        int[] a2 = a1;
-//        for (int i = 0; i < a2.length; ++i) {
-//            a2[i] ++;
-//        }
-//        for (int i = 0; i < a1.length; ++i) {
-//            System.out.println(a1[i]);
-//        }
+        String s1 = "0123456汉字";
 
-        /// 5  16  16  普通变量是"所有者"(真拷贝 赋值)   数组变量是"管理者"(指针拷贝赋值)
-        /*
-        * 数组变量是数组的管理者而非数组本身
-        * 数组必须创建出来然后交给数组变量来管理
-        * 数组变量之间的赋值是管理权限的赋予
-        * 数组变量之间的比较是判断是否管理同一个数组
-         */
-
-//        int[] a = {1,2,3};
-//        int[] b = a;
-//        int[] a = {1,2,3};
-//        int[] b = {1,2,3};
-
-//        int[] a = {1,2,3};
-//        int[] b = new int[a.length];
-//        for (int i = 0; i < b.length; i++) {
-//            b[i] = a[i];
-//        }
-//        for (int i = 0; i < b.length; i++) {
-//            System.out.println(b[i]);
-//        }
-//        System.out.println(a==b);
-
-        int[] a = {1,2,3};
-        int x = in.nextInt();
-        boolean found = false;
-        for ( int k: a) {
-            if (k == x) {
-                found = true;
-            }
-        }
-        System.out.println(found);
-
+//        String s2 = "abb";
+        System.out.println(s1.substring(2, 4));
+        System.out.println(s1.indexOf("56汉字"));
     }
 }
