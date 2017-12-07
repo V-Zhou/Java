3476d1cc29e75bcd39aea0271c293445f9a59441
v-zhou <v-zhou@hotmail.com>
2017-12-07 15:44:09 +0800

    java语言  入门 判断

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -5,10 +5,10 @@ public class VendingMachine {
         对象变量   默认为null 意味着没有连接任何对象
         可以给成员变量一个默认的值   甚至可以
          */
-    int price = 80;
-    int balance = f();
-    int total;
-
+    private int price = 80;
+    private int balance = f();
+    private int total;
+    String s = "";
     /* 构造函数
     * 如果一个成员函数的名字和类的名字完全相同, 则在创建这个类的每一个对象的时候会自动调用这个函数->构造函数
      * 这个函数不能有返回类型 写了返回值就不会被自动调用*/
@@ -26,6 +26,8 @@ public class VendingMachine {
 //        this(); //通过this()还可以调用其它构造函数 1.只能在构造函数里面出现 2. 只能出现在前面 3. 只能一次
 //        this.price = price;
 //    }
+
+
 
     VendingMachine(double price) { // 重载
         this(); //通过this()还可以调用其它构造函数 1.只能在构造函数里面出现 2. 只能出现在前面 3. 只能一次
